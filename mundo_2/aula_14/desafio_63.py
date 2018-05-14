13406df13d66681f58de4544e05e2e2bd0bfc9bf
guiemi <guiemi.teixeira@gmail.com>
2018-05-13 23:33:57 -0300

    Inserindo exercícios feitos em até 13/05

--- mundo_2/aula_14/desafio_63.py
+++ mundo_2/aula_14/desafio_63.py
@@ -2,5 +2,24 @@
 Escreva um programa que leia um número n inteiro qualquer
 e mostre na tela os primeiros n elementos e uma sequência de Fibonacci
 
-ex.: 0 --> 1 --> 1 --> 2 --> 3 --> 5 --> 8
+1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987, 1597, 2584 (...)
+
 '''
+
+# Fn + Fn+1 = Fn+2
+# Fn = Fn-1 + Fn-2
+
+user = int(input("Números de termos da sequência de Fibonacci que você deseja printar: \nfn = "))
+
+prevfn = 0
+fn = 1
+nextfn = 0
+index = 0
+
+for i in range(user + 1):
+    prevfn = fn
+    fn = nextfn
+    nextfn = (fn + (prevfn))
+    print("f{} = {}".format(index, fn))
+    index += 1
+print("Fim!")
