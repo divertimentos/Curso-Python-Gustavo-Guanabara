4af01712896a1b7e426b52fd9a71052b7c7aa68b
guiemi <guiemi.teixeira@gmail.com>
2018-05-14 01:17:55 -0300

    Atualizando o desafio 63

--- mundo_2/aula_14/desafio_63.py
+++ mundo_2/aula_14/desafio_63.py
@@ -5,9 +5,7 @@ e mostre na tela os primeiros n elementos e uma sequência de Fibonacci
 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987, 1597, 2584 (...)
 
 '''
-
-# Fn + Fn+1 = Fn+2
-# Fn = Fn-1 + Fn-2
+# Minha resolução:
 
 user = int(input("Números de termos da sequência de Fibonacci que você deseja printar: \nfn = "))
 
@@ -16,10 +14,29 @@ fn = 1
 nextfn = 0
 index = 0
 
-for i in range(user + 1):
+while index < user:
     prevfn = fn
     fn = nextfn
     nextfn = (fn + (prevfn))
     print("f{} = {}".format(index, fn))
     index += 1
 print("Fim!")
+
+'''
+# Resolução do Gustavo Guanabara:
+
+n = int(input("Termos: \n"))
+t1 = 0
+t2 = 1
+print("{} > {}".format(t1, t2), end="")
+cont = 3
+while cont <= n:
+    t3 = t1 + t2
+    print(" > {}".format(t3), end="")
+    t1 = t2
+    t2 = t3
+    cont += 1
+print(" > Fim!")
+'''
+
+# Achei meu código mais elegante que o dele.
