7f757ab0fd8e022d010de1150d3e0e2a198e549f
guiemi <guiemi.teixeira@gmail.com>
2018-05-17 02:48:33 -0300

    Inserindo o último desafio do mundo 2

---
+++ mundo_2/aula_15/desafio_71.2.py
@@ -0,0 +1,17 @@
+'''Crie um programa que simule o funcionamento de um caixa eletrônico.
+No início, pergunte ao usuário qual será o valor a ser sacado(inteiro)
+e o programa vai informar quantas cédulas de cada valor serão entregues.
+
+Obs.: Considere que o caixa possui cédulas de R$50, R$20, R$10 e R$1.
+'''
+
+num = int(input("Que valor você quer sacar? \n"))
+
+quantidade = soma = 0
+cedulas = [50, 20, 10, 1]
+
+while num > 0:
+    for i in cedulas:
+        cedulas = num // i
+        num = num % i
+        print(f"{cedulas} notas de R${i}")
