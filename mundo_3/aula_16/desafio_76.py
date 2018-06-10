f7ef7c3aaa628262a8819444a87cce2afaec2600
guiemi <guiemi.teixeira@gmail.com>
2018-06-09 23:06:38 -0300

    Atualização no exercício

--- mundo_3/aula_16/desafio_76.py
+++ mundo_3/aula_16/desafio_76.py
@@ -7,18 +7,23 @@ No final, mostre uma listagem de precos,
 organizando os dados em forma tabular
 '''
 
-listagem = ("pão", 1, "leite", 3.50, "frango", 10.90)
+listagem = (
+    "pão", 1,
+    "leite", 3.50,
+    "frango", 10.90
+      )
 
 print("=" * 35)
-print("Pau no cu da interface, na moralzinha.")
+print("Tela preta, letra verde.")
 print("=" * 35)
 
 produto = 0
 preco = 1
 
 for item in range(0, len(listagem)// 2):
-    print(f"{listagem[produto]} ***** R${listagem[preco]}")
+    print(f"{listagem[produto]:.<30} R${listagem[preco]:>7.2f}")
     preco += 2
     produto += 2
 
-# Esse é o programa mais gambiarrilson que eu já fiz, mas funcionou.
+# Esse é o programa mais gambiarrilson que eu já fiz, 
+# mas funcionou.
