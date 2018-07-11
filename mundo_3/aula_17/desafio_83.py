60e247441e18cb608ef80b8de37121aae60cb6d3
guiemi <guiemi.teixeira@gmail.com>
2018-07-11 15:51:14 -0300

    Nova tentativa de acertar o exercício sem auxílio

--- mundo_3/aula_17/desafio_83.py
+++ mundo_3/aula_17/desafio_83.py
@@ -6,13 +6,28 @@ está com os parênteses abertos e fechados na ordem correta.
 '''
 
 lista = []
-exp = str(input("Digite uma expressão: \n"))
 x = 0  # Contador
+exp = str(input("Digite uma expressão: \n"))
+print(f"Tamanho da expressão: {len(exp)}")
 
+while x < len(exp):
+    if exp[x] == "(":
+        lista.append(exp[x])
+    if exp[x] == ")":
+        if len(lista) > 0:
+            del lista[-1]
+            del x
+    x += 1
+print(f"Lista: {lista}")
+
+'''
 while x < len(exp):
     if exp[x] == "(":
         lista.append(x)
     if exp[x] == ")":
         if len(lista) > 0:
             lista = lista.pop(lista[-1])
-        
+    x += 1
+
+print(f"lista: {lista}")
+'''
