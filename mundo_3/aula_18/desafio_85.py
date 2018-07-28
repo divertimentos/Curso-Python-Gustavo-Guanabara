ff0ca772610dba24492324ea63c3487770cc7657
guiemi <guiemi.teixeira@gmail.com>
2018-07-27 23:29:51 -0300

    Exercício 85 resolvido

---
+++ mundo_3/aula_18/desafio_85.py
@@ -0,0 +1,21 @@
+'''
+Crie um programa onde o usuário possa digitar
+sete valores numéricos
+e cadastre-os em uma lista única
+que mantenha separados os valores pares e ímpares.
+
+No final, mostre os valores pares e ímpares
+em ordem crescente.
+'''
+
+lista = [[],[]]
+
+for num in range(1, 8):
+    numero = int(input((f"Digite um número ({num}/7): \n")))
+    if numero % 2 == 0:
+        lista[0].append(numero)  # números pares
+    else:
+        lista[1].append(numero)  # números ímpares
+
+print(f"Números pares em ordem crescente: {sorted(lista[0])}")
+print(f"Números ímpares em ordme crescente: {sorted(lista[1])}")
