85d5c00fa8d0b12d12698b2f112c3649debcd9df
guiemi <guiemi.teixeira@gmail.com>
2018-08-05 08:32:40 -0300

    Adiciona comentários ao código do exercício

--- mundo_3/aula_18/desafio_86.py
+++ mundo_3/aula_18/desafio_86.py
@@ -5,14 +5,16 @@ e preencha com valores lidos pelo teclado.
 No final, mostre a matriz na tela com a formatação correta
 '''
 
+# Declarando variáveis
 lista = list()
 listb = list()
 
-for i in range(0, 3):
-    for j in range(0, 3):
+
+for i in range(0, 3):  # Para cada linha
+    for j in range(0, 3):  # Para cada coluna em cada linha
         listb.append(int(input(f"Digite um valor para [{i}, {j}]: \n")))
-    
-    lista.append(listb[:])
+
+    lista.append(listb[:])  # Copie a linha para listb
     listb.clear()
 
 for l in lista:
