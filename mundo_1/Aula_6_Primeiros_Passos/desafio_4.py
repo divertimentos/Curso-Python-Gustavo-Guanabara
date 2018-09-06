5d99cba491ad7690416b7c4d913b9e1606912d96
guiemi <guiemi.teixeira@gmail.com>
2018-09-06 00:05:01 -0300

    Pep 8

--- mundo_1/Aula_6_Primeiros_Passos/desafio_4.py
+++ mundo_1/Aula_6_Primeiros_Passos/desafio_4.py
@@ -1,4 +1,6 @@
-# Faça um programa que leia algo pelo teclado e mostre na tela o seu tipo primitivo e todas as informações possíveis sobre ele
+# Faça um programa que leia algo pelo teclado 
+# e mostre na tela o seu tipo primitivo 
+# e todas as informações possíveis sobre ele
 
 entrada = input("Digite algo: \n")
 print("Tipo primitivo: {}.".format(type(entrada)))
