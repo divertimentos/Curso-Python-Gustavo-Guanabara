afa4f504c9855ce1b100560bfcf64e7f5c4d58b8
guiemi <guiemi.teixeira@gmail.com>
2018-11-25 00:17:50 -0200

    Completa o exercício 88.

--- mundo_3/aula_18/desafio_88.py
+++ mundo_3/aula_18/desafio_88.py
@@ -1,34 +1,31 @@
 '''
 Faça um programa que ajude um jogador da MEGA SENA a criar palpites.
 
-O programa vai perguntar quantos jogos serão gerados 
+O programa vai perguntar quantos jogos serão gerados
 e vai sortear 6 números entre 1 e 60 para cada jogo,
 cadastrando tudo em uma lista composta.
 '''
 from random import randint
 from time import sleep
+lista = list()
+jogos = list()
+quantidade = int(input("Quantos jogos você quer que eu sorteie? \n"))
+total = 1
 
-palpites = list()
+while total <= quantidade:
+    counter = 0
+    while True:
+        number = randint(1, 60)
+        if number not in lista:
+            lista.append(number)
+            counter += 1
+        if counter >= 6:
+            break
+    lista.sort()
+    jogos.append(lista[:])
+    lista.clear()
+    total += 1
 
-n = int(input("Quantos jogos? :\n"))
-
-'''
-for i in range(n):
-    for j in range(6):
-        sleep(1)
-        print(f"teste{j}.")
-        palpites.append(randint(1, 60))
+for i, l in enumerate(jogos):
+    print(f"jogo {i + 1}: {l}")
     sleep(1)
-    print(f"Jogo {i+1}: {palpites}")
-    palpites.clear()
-'''
-'''
-for jogo in range(n):
-    while len(palpites) < 6:
-        for num in palpites:
-            if randint(1, 60) != num:
-                palpites.append(randint(1, 60))
-    sleep(1)
-    print(f"Jogo: {palpites}.")
-'''
-
