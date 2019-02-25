7cd37f8cdf13442f3c6e4d80915be5860b0ba362
guiemi <guiemi.teixeira@gmail.com>
2019-02-25 11:54:20 -0300

    Escreve duas funções:
    A primeira retorna um nome
    A segunda pega esse nome, insere-o como cabeça de uma lista, pega duas
    notas e as insere na lista

--- mundo_3/aula_18/desafio_89.py
+++ mundo_3/aula_18/desafio_89.py
@@ -5,7 +5,26 @@ e guarde tudo em uma lista composta.
 No final, mostre um boletim contendo a média de cada um
 e permita que o usuário possa mostrar as notas de cada aluno individualmente
 
-Obs.: esse exercício eu nem sei se é difícil ou fácil, trabalhoso ou não;
-ele é difícil de entender.
 
 '''
+
+infos_alunos = list()
+
+
+def get_name():
+    name = input("Nome do aluno: \n")
+    return name
+
+
+def get_grades(student_name):
+    grades = list()
+    counter = 1
+    grades.append(student_name)
+    while counter < 3:
+        grade = int(input(f"Nota {counter}: \n"))
+        grades.append(grade)
+        counter += 1
+    return grades
+
+
+print(get_grades(get_name()))
