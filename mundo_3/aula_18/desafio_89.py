55d6270129d656126300329c2e6304a7e8c244c3
guiemi <guiemi.teixeira@gmail.com>
2019-02-25 16:15:41 -0300

    Implementa uma função que printa a média de notas de cada aluno inserido

--- mundo_3/aula_18/desafio_89.py
+++ mundo_3/aula_18/desafio_89.py
@@ -1,19 +1,25 @@
-'''
+"""
 Crie um programa que leia nome e duas notas de vários alunos
 e guarde tudo em uma lista composta.
 
 No final, mostre um boletim contendo a média de cada um
 e permita que o usuário possa mostrar as notas de cada aluno individualmente
 
+"""
 
-'''
 
-infos_alunos = list()
+"""
+Cria um programa que lê nome e duas notas de vários alunos
+e guarda-os numa lista completa.
+"""
+
+from statistics import mean
+
+infos_students = list()
 
 
 def get_name():
-    name = input("Nome do aluno: \n")
-    return name
+    return input("Nome do aluno: \n")
 
 
 def get_grades(student_name):
@@ -21,10 +27,41 @@ def get_grades(student_name):
     counter = 1
     grades.append(student_name)
     while counter < 3:
-        grade = int(input(f"Nota {counter}: \n"))
+        grade = int(input(f"{counter}ª nota: \n"))
         grades.append(grade)
         counter += 1
-    return grades
+    infos_students.append(grades)
+    return infos_students
 
 
-print(get_grades(get_name()))
+def display_infos():
+    return infos_students
+
+
+while True:
+    get_grades(get_name())
+    question = input("Deseja adiciona mais um aluno? \n").lower()
+    if question[0] == "n":
+        # print(display_infos())
+        break
+
+"""
+Mostra um boletim contendo a média de cada um 
+e permita que o usuário possa mostrar as notas de cada aluno individualmente
+"""
+
+
+def calculate_mean(students_infos):
+    students_means = list()
+    for item in students_infos:
+        students_means.append(mean(item[1:]))
+    return students_means
+
+
+def display_school_report():
+    pass
+
+
+# display_school_report(display_infos())
+
+print(calculate_mean(display_infos()))
