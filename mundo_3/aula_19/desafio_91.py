26e4dac5b9f9894599bbd01bb1d8f328d2a92db6
guiemi <guiemi.teixeira@gmail.com>
2019-04-04 16:49:45 -0300

    Resolve o desafio 91

--- mundo_3/aula_19/desafio_91.py
+++ mundo_3/aula_19/desafio_91.py
@@ -6,3 +6,26 @@ Guarde esses resultados em um dicionário.
 No final, coloque esse dicionário em ordem,
 sabendo que o vencedor tirou o maior número no dado
 """
+
+from random import randint
+
+players = dict()
+
+print("Valores sorteados: \n")
+for i in range(4):
+    jogada = randint(1, 6)
+    players[f"jogador {i + 1}"] = jogada
+
+    print(f"O jogador {i + 1} tirou {jogada} no dado.")
+
+print("\nRanking dos jogadores:")
+
+players_list = sorted(list(zip(players.values(), players.keys())), reverse=True)
+# print(players_list)
+
+counter = 0
+print(players_list)
+
+for player in players_list:
+    print(f"{counter + 1}º lugar: {players_list[counter][1]} com {players_list[counter][0]}")
+    counter += 1
