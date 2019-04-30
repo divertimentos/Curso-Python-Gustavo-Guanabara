147f5dadc3ffcf4595065886f68adc94c3c0fdd9
guiemi <guiemi.teixeira@gmail.com>
2019-04-30 18:26:39 -0300

    WIP: solving desafio_92.py

--- mundo_3/aula_19/desafio_92.py
+++ mundo_3/aula_19/desafio_92.py
@@ -9,3 +9,25 @@ a pessoa vai se aposentar.
 Obs.: aposentadoria em 35 anos de contribuição.
 """
 
+worker_infos = dict()
+
+name = input("Digite um nome: \n")
+birthday = input("Data de nascimento: \n")
+ctps = int(input("Número da carteira de trabalho (CTPS): \n"))
+
+retirement_age = int()
+age = int()
+
+
+worker_infos["name"] = name
+worker_infos["birthday"] = birthday
+worker_infos["ctps"] = ctps
+
+if ctps != 0:
+    hiring_date = input("Data de contratação: \n")
+    salary = int(input("Salário: \nR$ "))
+    
+    worker_infos["hiring date"] = hiring_date
+    worker_infos["salary"] = salary
+
+print(worker_infos)
