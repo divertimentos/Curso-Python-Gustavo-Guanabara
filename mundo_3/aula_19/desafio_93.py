4eab8690725f1d4aa5e1d7f91c7e3439bd64a6bc
guiemi <guiemi.teixeira@gmail.com>
2019-11-09 19:37:32 -0300

    Resolve o exercício 93

--- mundo_3/aula_19/desafio_93.py
+++ mundo_3/aula_19/desafio_93.py
@@ -5,3 +5,26 @@ Depois vai ler a quantidade de gols feitos em cada partida.
 No final, tudo isso será guardado em um dicionário,
 incluindo o total de gols feitos durante o campeonato.
 """
+aproveitamento = {
+    'nome': None,
+    'partidas': None,
+    'gols': 0,
+}
+
+# Nome
+nome = input("Qual o nome do jogador? \n" )
+aproveitamento['nome'] = nome
+
+# Partidas
+partidas = int(input("Partidas jogadas: \n"))
+aproveitamento['partidas'] = partidas
+
+# Gols em cada partida
+for partida in range(partidas):
+    gols_partida = int(input(f"Quantos gols o {nome} fez na partida {partida+1}? \n"))
+    aproveitamento['gols'] += gols_partida
+
+
+print(
+    f"O jogador {nome} jogou {partidas} partidas e possui um total de {aproveitamento['gols']} gols!"
+)
