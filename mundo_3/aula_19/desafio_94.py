ccd2b4667c6048ac8428e5b395883255a7e59040
guiemi <guiemi.teixeira@gmail.com>
2020-02-17 00:24:46 -0300

    Inicia o divertimento_94

--- mundo_3/aula_19/desafio_94.py
+++ mundo_3/aula_19/desafio_94.py
@@ -9,3 +9,33 @@ b. A média de idade do grupo
 c. uma lista com todas as mulheres
 d. uma lista com todas as pessoas com idade acima da média.
 """
+
+personal_info = dict()
+general_info = list()
+
+while True:
+    personal_info['nome'] = input("Nome: \n")
+    personal_info['idade'] = input("Idade: \n")
+    general_info.append(personal_info)
+
+
+    leave = input("Continuar? [S / N] \n")
+    if leave[0] == "N":
+        break
+print(general_info)
+
+# while True:
+#     name_dict['nome'] = input("Nome: \n")
+    
+#     sex = input("Sexo [M / F]: \n")
+#     sex = sex[0:1].upper()
+#     sex_dict['sex'] = sex
+    
+#     age_dict['age'] = input("Idade: \n")
+
+#     people_list.append([name_dict, sex_dict, age_dict])
+#     leave = input("Continuar? [S / N] \n")
+#     if leave[0] == "N":
+#         break
+
+# print(general_list)
