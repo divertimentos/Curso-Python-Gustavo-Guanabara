dbefdb879644c4147ddfc122e4d6bd6aa9afbdb4
guiemi <guiemi.teixeira@gmail.com>
2020-02-19 17:33:56 -0300

    Lista apenas as mulheres

--- mundo_3/aula_19/desafio_94.py
+++ mundo_3/aula_19/desafio_94.py
@@ -14,21 +14,23 @@ person_dict = dict()
 general_info = list()
 names = list()
 ages = list()
+women = list()
 
 counter = 0
 
 while True:
     # person_dict['name'] = input("Nome: \n")
 
-    # sex = input("Sexo  [M/F]: \n")
-    # sex = sex[0:1].upper()
-    # person_dict['sex'] = sex
-    person_dict['age'] = float(input("Idade: \n"))
+    sex = input("Sexo [M/F]: \n")
+    sex = sex[0].upper()
+    person_dict['sex'] = sex
+    # person_dict['age'] = float(input("Idade: \n"))
     
 
     general_info.append(person_dict.copy())
 
     leave = input("Continuar? [S/N] \n")
+    leave = leave.upper()
     if leave[0] == "N":
         break
 
@@ -40,10 +42,17 @@ while True:
 # print(f"Pessoas cadastradas: {len(names)}")
 
 # b.: a média de idade do grupo:
-for dictionary in general_info:
-    ages.append(dictionary.get("age"))
-print(sum(ages)/len(ages))
+# for dictionary in general_info:
+#     ages.append(dictionary.get("age"))
+# print(sum(ages)/len(ages))
 
 # c.: uma lista com todas as mulheres:
+for dictionary in general_info:
+    woman = dictionary.get("sex", "Não encontrado!")
+    if woman == "F":
+        women.append(woman)
+print(women)
+
+
 
 # d.: uma lista com todas as pessoas com idade acima da média:
