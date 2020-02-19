09f6c0ce113a2e9e36cfa0a06b252c4827f8643f
guiemi <guiemi.teixeira@gmail.com>
2020-02-19 17:01:25 -0300

    Calcula a médie de idade do grupo

--- mundo_3/aula_19/desafio_94.py
+++ mundo_3/aula_19/desafio_94.py
@@ -13,14 +13,18 @@ d. uma lista com todas as pessoas com idade acima da média.
 person_dict = dict()
 general_info = list()
 names = list()
+ages = list()
+
 counter = 0
 
 while True:
-    person_dict['name'] = input("Nome: \n")
+    # person_dict['name'] = input("Nome: \n")
 
     # sex = input("Sexo  [M/F]: \n")
     # sex = sex[0:1].upper()
     # person_dict['sex'] = sex
+    person_dict['age'] = float(input("Idade: \n"))
+    
 
     general_info.append(person_dict.copy())
 
@@ -29,12 +33,16 @@ while True:
         break
 
 # a.: quantas pessoas foram cadastradas:
-for dictionary in general_info:
-    for key, value in (dictionary.items()):
-        names.append(value)
-print(f"Pessoas cadastradas: {len(names)}")
+
+# for dictionary in general_info:
+#     for key, value in dictionary.items():
+#         names.append(value)
+# print(f"Pessoas cadastradas: {len(names)}")
 
 # b.: a média de idade do grupo:
+for dictionary in general_info:
+    ages.append(dictionary.get("age"))
+print(sum(ages)/len(ages))
 
 # c.: uma lista com todas as mulheres:
 
