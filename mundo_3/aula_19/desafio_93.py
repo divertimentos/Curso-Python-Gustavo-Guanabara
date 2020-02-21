86885bad6574a9bc569b30d64c95d546b406606b
guiemi <guiemi.teixeira@gmail.com>
2020-02-21 19:13:32 -0300

    Refaz o código, melhorando o comportamento dos dicionários

--- mundo_3/aula_19/desafio_93.py
+++ mundo_3/aula_19/desafio_93.py
@@ -5,26 +5,31 @@ Depois vai ler a quantidade de gols feitos em cada partida.
 No final, tudo isso será guardado em um dicionário,
 incluindo o total de gols feitos durante o campeonato.
 """
-aproveitamento = {
-    'nome': None,
-    'partidas': None,
-    'gols': 0,
-}
+# aproveitamento = {
+#     'nome': None,
+#     'partidas': None,
+#     'gols': 0,
+# }
+
+aproveitamento = dict()
+gols_total = 0
 
 # Nome
-nome = input("Qual o nome do(a) jogador(a)? \n" )
-aproveitamento['nome'] = nome
+aproveitamento['nome'] = input("Qual o nome do(a) jogador(a)? \n" )
 
 # Partidas
-partidas = int(input("Partidas jogadas: \n"))
-aproveitamento['partidas'] = partidas
+aproveitamento['partidas'] = int(input("Partidas jogadas: \n"))
 
 # Gols em cada partida
-for partida in range(partidas):
-    gols_partida = int(input(f"Quantos gols o(a) jogador(a) {nome} fez na partida {partida+1}? \n"))
-    aproveitamento['gols'] += gols_partida
+for partida in range(aproveitamento['partidas']):
+    gols_partida = int(input(f"Quantos gols o jogador {aproveitamento['nome']} fez na partida {partida +1}? \n"))
+    gols_total += gols_partida
+aproveitamento['gols_total'] = gols_total
 
+# print(
+#     f"O(A) jogador(a) {nome} jogou {partidas} partidas e possui um total de {aproveitamento['gols']} gols!"
+# )
 
 print(
-    f"O(A) jogador(a) {nome} jogou {partidas} partidas e possui um total de {aproveitamento['gols']} gols!"
+    f"O jogador {aproveitamento['nome']} jogou {aproveitamento['partidas']} partidas e fez um total de {aproveitamento['gols_total']} gols."
 )
