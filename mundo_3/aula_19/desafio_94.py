6a840e4d129d82c9a344ecfb94bf46f9d3a3eb5d
guiemi <guiemi.teixeira@gmail.com>
2020-02-21 12:21:38 -0300

    Unifies all loops into only one

--- mundo_3/aula_19/desafio_94.py
+++ mundo_3/aula_19/desafio_94.py
@@ -14,44 +14,44 @@ person_dict = dict()
 general_info, names, names_quantity, ages, women, above_mean = list(), list(), list(), list(), list(), list()
 
 while True:
+    # recebe nome:
     person_dict['name'] = input("Nome: \n")
 
+    # recebe sexo:
     sex = input("Sexo [M/F]: \n")
     sex = sex[0].upper()
     person_dict['sex'] = sex
 
+    # recebe idade:
     person_dict['age'] = float(input("Idade: \n"))
     
-
+    # insere os dicionários na lista:
     general_info.append(person_dict.copy())
 
+    # continuar ou sair?
     leave = input("Continuar? [S/N] \n")
     leave = leave.upper()
     if leave[0] == "N":
         break
 
-# a.: quantas pessoas foram cadastradas:
 for dictionary in general_info:
+    # a.: quantas pessoas foram cadastradas:
     names_quantity.append(dictionary.get("name"))
-print(f"Pessoas cadastradas: {len(names_quantity)}")
 
-# b.: a média de idade do grupo:
-for dictionary in general_info:
+    # b.: a média de idade do grupo:
     ages.append(dictionary.get("age"))
     age_mean = sum(ages)/len(ages)
-print(f"Idade média do grupo: {age_mean:.1f} anos.")
 
-# c.: uma lista com todas as mulheres:
+    # d.: uma lista com todas as pessoas com idade acima da média:
+    if dictionary.get("age") > (age_mean):
+        above_mean.append(dictionary.get("name"))
 
-for dictionary in general_info:
+    # c.: uma lista com todas as mulheres:
     for key, value in dictionary.items():
         if value == "F":
             women.append(dictionary.get("name"))
-print(f"Mulheres do grupo: {women}")
 
-
-# d.: uma lista com todas as pessoas com idade acima da média:
-for dictionary in general_info:
-    if dictionary.get("age") > (age_mean):
-        above_mean.append(dictionary.get("name"))
+print(f"Pessoas cadastradas: {len(names_quantity)}")
+print(f"Idade média do grupo: {age_mean:.1f} anos.")
 print(f"Pessoas com idade acima da média: {above_mean}")
+print(f"Mulheres do grupo: {women}")
