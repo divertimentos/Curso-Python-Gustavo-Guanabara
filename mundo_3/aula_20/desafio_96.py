33481dcb21b3e365e62cd4389c5356102201af6d
guiemi <guiemi.teixeira@gmail.com>
2020-03-01 13:23:58 -0300

    Resolve o exercício 96

--- mundo_3/aula_20/desafio_96.py
+++ mundo_3/aula_20/desafio_96.py
@@ -1,2 +1,18 @@
 """Faça um programa que tenha uma função chamada area(), que receba dimensões de um terreno retangular (largura e comprimento) e mostre a área do terreno
 """
+def cabecalho(titulo):
+    print("-" * len(titulo))
+    print(f"{titulo}")
+    print("-" * len(titulo))
+
+cabecalho("Controle de Terrenos")
+
+def area(largura, comprimento):
+    print(f"A área de um terreno '{largura}x{comprimento}' é de {largura * comprimento}m².")
+
+largura = float(input("Largura (m): \n"))
+comprimento = float(input("Comprimento: \n"))
+
+
+
+area(largura, comprimento)
