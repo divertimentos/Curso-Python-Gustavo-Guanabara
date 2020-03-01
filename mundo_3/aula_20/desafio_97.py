0a27e210e5401051a1d3505e31772c8a1388bebe
guiemi <guiemi.teixeira@gmail.com>
2020-03-01 13:31:17 -0300

    Resolve o exercício 97

--- mundo_3/aula_20/desafio_97.py
+++ mundo_3/aula_20/desafio_97.py
@@ -1,4 +1,6 @@
-"""Faça um programa que tenha uma função chamada escreva(), que receba um texto qualquer como parâmetro e mostre uma mensagem com tamanho adaptável.
+"""Faça um programa que tenha uma função chamada escreva(), 
+que receba um texto qualquer como parâmetro 
+e mostre uma mensagem com tamanho adaptável.
 
 Ex:
 escreva("Olá, Mundo!")
@@ -9,3 +11,12 @@ Saída:
 Olá, Mundo!
 ~~~~~~~~~~~
 """
+
+def escreva(texto):
+    print("~" * len(texto))    
+    print(f"{texto:^5}")
+    print("~" * len(texto))    
+
+escreva("Gustavo Guanabara")
+escreva("Curso de Python no YouTube")
+escreva("CeV")
