87c37f60e38eb73be942b1d6b3fa13a37256e0b1
Guilherme <guiemi.teixeira@gmail.com>
2020-03-03 12:44:31 -0300

    Resolve o exercício 100

--- mundo_3/aula_20/desafio_100.py
+++ mundo_3/aula_20/desafio_100.py
@@ -1,3 +1,32 @@
+from random import randint
+from time import sleep
+
 """
-Faça um programa que tenha uma lista chamada números e duas funções chamadas sorteia() e soma_par(). A primeira função vai sortear 5 números e vai colocá-los dentro da lista e a segunda função vai mostrar a soma entre todos os valores pares sorteados pela função anterior.
+Faça um programa que tenha uma lista chamada números e duas funções chamadas sorteia() e soma_par(). 
+A primeira função vai sortear 5 números e vai colocá-los dentro da lista 
+e a segunda função vai mostrar a soma entre todos os valores pares sorteados pela função anterior.
 """
+
+def sorteia():
+    for n in range(0, 5):
+        numeros.append(randint(0, 99))
+    print(f"Sorteando {len(numeros)} valores da lista: ", end="")
+    for i in numeros:
+        print(f"{i} ", end="")
+        sleep(0.5)
+
+
+def soma_par(lista_numeros):
+    pares = list()
+    for n in lista_numeros:
+        if n % 2 == 0:
+            pares.append(n)
+    print(f"\nOs valores pares da lista são: ", end="")
+    print(f"{pares}", end="")
+    print(f"E a soma é: {sum(pares)}")
+
+numeros = list()
+
+sorteia()
+soma_par(numeros)
+
