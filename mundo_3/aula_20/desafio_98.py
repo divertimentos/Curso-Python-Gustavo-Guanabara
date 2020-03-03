4c24b57e5f7251687aba8045595ac37837d08c34
Guilherme <guiemi.teixeira@gmail.com>
2020-03-03 12:44:52 -0300

    WIP: corrigindo  o exercício 98

--- mundo_3/aula_20/desafio_98.py
+++ mundo_3/aula_20/desafio_98.py
@@ -1,3 +1,5 @@
+from time import sleep
+
 """
 Faça um programa que tenha uma função chamada contador(), que recebe três parâmetros:
 início, fim e passo
@@ -8,14 +10,36 @@ Seu programa tem que realizar três contagens através da função criada
 a) de 1 até 10, de 1 em 1
 b) de 10 até 0, de 2 em 2
 c) Uma contagem personalizada
+
+--> print(f"{valor} ", end="")  # Exemplo de print espaçado
 """
 
-# exemplo de print que o desafio quer:
-def contador(*num):
-    for valor in num:
-        print(f"{valor} ", end="")
-    print("Fim!")
+def contador():
+    # print("Contagem de 1 até 10 de 1 em 1:")
+    # for i in range(1, 11, 1):
+    #     print(f"{i} ", end="")
+    #     sleep(0.3)
+    
+    # print(" ", end="\n")
+    
+    # print("Contagem de 10 até 0 de 2 em 2:")
+    # for j in range(10, 0, -2):
+    #     print(f"{j} ", end="")
+    #     sleep(0.2)
+    
+    print("Agora é a sua vez: \n")
+    inicio = int(input("Início: \n"))
+    fim = int(input("Fim: \n"))
+    passo = int(input("Passo: \n"))
 
-contador(2, 1, 7)
-contador(8, 0)
-contador(4, 4, 7, 5, 2)
+    print(f"Contagem de {inicio} até {fim} de {passo} em {passo}")
+    if inicio < fim:
+        for k in range(inicio, fim, passo):
+            print(f"{k} ", end="")
+            sleep(0.1)
+    else:
+        for k in range(fim, inicio, passo):
+            print(f"{k} ", end="")
+            sleep(0.1)
+
+contador()
