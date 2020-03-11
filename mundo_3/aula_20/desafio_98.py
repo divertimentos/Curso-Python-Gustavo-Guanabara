b782b795c3622d1586acf2f28a391deffdad283d
guiemi <guiemi.teixeira@gmail.com>
2020-03-11 00:15:16 -0300

    Corrige o divertimento 98

--- mundo_3/aula_20/desafio_98.py
+++ mundo_3/aula_20/desafio_98.py
@@ -14,12 +14,33 @@ c) Uma contagem personalizada
 --> print(f"{valor} ", end="")  # Exemplo de print espaçado
 """
 
-def contador():
-    # print("Contagem de 1 até 10 de 1 em 1:")
-    # for i in range(1, 11, 1):
-    #     print(f"{i} ", end="")
-    #     sleep(0.3)
-    
+def contador(inicio, fim, passo):
+    if passo < 0:
+        passo *= -1
+    if passo == 0:
+        passo = 1
+
+    print(f"Contagem de {inicio} até {fim} de {passo} em {passo}.")
+
+    if inicio < fim:
+        counter = inicio
+        while counter <= fim:
+            print(f"{counter} ", end="")
+            sleep(0.2)
+            counter += passo
+        print("Fim")
+    else:
+        counter = inicio
+        while counter >= fim:
+            print(f"{counter} ", end="")
+            sleep(0.2)
+            counter -= passo
+        print("Fim!")
+
+inicio = int(input("Início: \n"))
+fim = int(input("Fim: \n"))
+passo = int(input("Passo: \n"))
+
     # print(" ", end="\n")
     
     # print("Contagem de 10 até 0 de 2 em 2:")
@@ -27,19 +48,21 @@ def contador():
     #     print(f"{j} ", end="")
     #     sleep(0.2)
     
-    print("Agora é a sua vez: \n")
-    inicio = int(input("Início: \n"))
-    fim = int(input("Fim: \n"))
-    passo = int(input("Passo: \n"))
+    # print("Agora é a sua vez: \n")
+    # inicio = int(input("Início: \n"))
+    # fim = int(input("Fim: \n"))
+    # passo = int(input("Passo: \n"))
 
-    print(f"Contagem de {inicio} até {fim} de {passo} em {passo}")
-    if inicio < fim:
-        for k in range(inicio, fim, passo):
-            print(f"{k} ", end="")
-            sleep(0.1)
-    else:
-        for k in range(fim, inicio, passo):
-            print(f"{k} ", end="")
-            sleep(0.1)
+    # print(f"Contagem de {inicio} até {fim} de {passo} em {passo}")
+    # if inicio < fim:
+    #     for k in range(inicio, fim, passo):
+    #         print(f"{k} ", end="")
+    #         sleep(0.1)
+    # else:
+    #     for k in range(fim, inicio, passo):
+    #         print(f"{k} ", end="")
+    #         sleep(0.1)
 
-contador()
+# contador(1, 10, 1)
+# contador(10, 0, 2)
+contador(inicio, fim, passo)
