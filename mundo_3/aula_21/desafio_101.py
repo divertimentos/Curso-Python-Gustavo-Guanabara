4bd0301d1308bfd6b83d8948430fc57b3ca382a2
guiemi <guiemi.teixeira@gmail.com>
2020-03-15 13:54:07 -0300

    Resolve o desafio 101

--- mundo_3/aula_21/desafio_101.py
+++ mundo_3/aula_21/desafio_101.py
@@ -1,4 +1,23 @@
+from datetime import date 
 """
 Crie um programa que tenha uma função chamada voto()
-que vai receber como parâmetro o ano de nascimento de uma pessoa, retornando um valor literal indicando se uma pessoa tem voto NEGADO, OPCIONAL ou OBRIGATORIO nas eleições.
+que vai receber como parâmetro o ano de nascimento de uma pessoa, 
+retornando um valor literal indicando se uma pessoa tem voto 
+NEGADO, OPCIONAL ou OBRIGATORIO nas eleições.
 """
+
+def voto(birthyear):
+    current_year = date.today().year
+    age = current_year - birthyear
+    if age < 16:
+        print(f"Com {age} anos de idade: NEGADO")
+    elif age < 18 or age >= 65:
+        print(f"Com {age} anos de idade: OPCIONAL")
+    elif age < 65:
+        print(f"Com {age} anos de idade: OBRIGATÓRIO")
+        
+
+
+
+ano_nasc = int(input("Em qual ano você nasceu? \n"))
+voto(ano_nasc)
