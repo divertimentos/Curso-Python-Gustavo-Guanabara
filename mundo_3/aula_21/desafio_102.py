bae27fef02125a111911690551574fdffb1a9ac4
guiemi <guiemi.teixeira@gmail.com>
2020-03-16 00:25:20 -0300

    Resolve o exercício 102

--- mundo_3/aula_21/desafio_102.py
+++ mundo_3/aula_21/desafio_102.py
@@ -1,5 +1,23 @@
 """
 Crie um programa que tenha uma função fatorial() que receba dois parâmetros:
 o primeiro que indique o número a calcular
-e o outro chamado show, que será um valor lógico (opcional) indicando se será mostrado ou não na tela o processo de cálculo do fatorial.
+e o outro chamado show, que será um valor lógico (opcional) 
+indicando se será mostrado ou não na tela o processo de cálculo do fatorial.
 """
+
+def fatorial(num, show=False):
+    fatorial = 1
+    for i in range(num, 0, -1):
+        if show:
+            print(i, end="")
+            if i > 1:
+                print(" x ", end="")
+            else:
+                print(" = ", end="")
+
+    
+
+        fatorial *= i
+    return fatorial
+
+print(fatorial(5, show=True))
--- mundo_3/aula_21/divertimentos.py
+++ mundo_3/aula_21/divertimentos.py
@@ -60,25 +60,25 @@
 
 ### PARTE PRATICA DA AULA:
 
-# def fatorial(num=1):
-#     fatorial = 1
-#     for i in range(num, 0, -1):
-#         fatorial *= i
-#     return fatorial
+def fatorial(num=1):
+    fatorial = 1
+    for i in range(num, 0, -1):
+        fatorial *= i
+    return fatorial
 
-# f1 = fatorial(5)
-# f2 = fatorial(4)
-# f3 = fatorial()
+f1 = fatorial(5)
+f2 = fatorial(4)
+f3 = fatorial()
 
 # print(f"Os resultados são: {f1}, {f2} e {f3}")
 
-def par(n=0):
-    if n % 2 == 0:
-        return True
-    else:
-        return False
-num = int(input("Digite um número: \n"))
-if par(num):
-    print(f"É par!")
-else:
-    print(f"É ímpar!")
+# def par(n=0):
+#     if n % 2 == 0:
+#         return True
+#     else:
+#         return False
+# num = int(input("Digite um número: \n"))
+# if par(num):
+#     print(f"É par!")
+# else:
+#     print(f"É ímpar!")
