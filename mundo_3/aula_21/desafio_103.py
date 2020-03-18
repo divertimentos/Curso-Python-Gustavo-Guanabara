ab090992ec311d5b4185beccd65a2730651129af
guiemi <guiemi.teixeira@gmail.com>
2020-03-18 13:37:09 -0300

    Corrige o problema dos tipos do código

--- mundo_3/aula_21/desafio_103.py
+++ mundo_3/aula_21/desafio_103.py
@@ -6,13 +6,17 @@ O programa deverá ser capaz de mostrar a ficha do jogador, mesmo que algum dado
 """
 
 def ficha(nome_jogador, numero_gols):
-    if len(nome_jogador) == 0:
-        nome_jogador = "<desconhecido>"
-    if len(numero_gols) == 0:
-        numero_gols = 0
-
     return f"O jogador {nome_jogador.upper()} fez {numero_gols} gols."
+
 nome = str(input("Nome do jogador: \n"))
-gols = (input("Número de gols: \n"))
+if len(nome) == 0:
+    nome = "<desconhecido>"
+
+gols = str(input("Número de gols: \n"))
+if gols.isnumeric():
+    gols = int(gols)
+else:
+    gols = 0
+
 
 print(ficha(nome, gols))
