96e016a26eff3ac651685e45572ee66efd5dfc8e
guiemi <guiemi.teixeira@gmail.com>
2020-03-18 12:58:47 -0300

    Resolve o exercício 103 (com um probleminha de tipos)

--- mundo_3/aula_21/desafio_103.py
+++ mundo_3/aula_21/desafio_103.py
@@ -4,3 +4,15 @@ o nome de um jogador e quantos gols ele marcou.
 
 O programa deverá ser capaz de mostrar a ficha do jogador, mesmo que algum dado não tenha sido informado corretamente.
 """
+
+def ficha(nome_jogador, numero_gols):
+    if len(nome_jogador) == 0:
+        nome_jogador = "<desconhecido>"
+    if len(numero_gols) == 0:
+        numero_gols = 0
+
+    return f"O jogador {nome_jogador.upper()} fez {numero_gols} gols."
+nome = str(input("Nome do jogador: \n"))
+gols = (input("Número de gols: \n"))
+
+print(ficha(nome, gols))
