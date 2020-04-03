5b3799a7e4465372a1ce1690c2fc7647215797ea
guiemi <guiemi.teixeira@gmail.com>
2020-04-03 13:28:43 -0300

    Resolve o exercício*
    Algo no meu terminal não me permitiu completar esse exercício. Mas eu
    soube fazer e a correção do Guanabara mostra basicamente o que eu faria
    para resolver.
    
    Ele mostra como usar cores, algo que eu simplesmente ignorei. Considero,
    então, o desafio completado.

--- mundo_3/aula_21/desafio_106.py
+++ mundo_3/aula_21/desafio_106.py
@@ -1,3 +1,4 @@
+from time import sleep
 """
 Faça um minissistema que utilize o interactive help do Python
 
@@ -7,3 +8,25 @@ Quando o usuário digitar a palavra "FIM", o programa se encerrará!.
 
 OBS: use cores.
 """
+def escreva(texto):
+    tamanho = len(texto) + 4
+    print("~" * tamanho)    
+    print(f"  {texto}")
+    print("~" * tamanho)    
+
+def pyhelp(comando):
+    help(comando)    
+
+def default_loop():
+    while True:
+        escreva("Sistema de ajuda PyHELP")
+        funcao_ou_lib = input("Função ou Biblioteca > ")
+
+        if funcao_ou_lib[0].lower() == "f":
+            break
+        else:
+            # escreva(f"Acessando o manual do comando {funcao_ou_lib}")
+            # sleep(1)
+            pyhelp(funcao_ou_lib)
+
+default_loop()
