95f4bc2df70d73036fcbb5abf7866bb91a4357e6
Guilherme Teixeira <24235344+vibraniumdev@users.noreply.github.com>
2021-11-06 16:52:37 -0300

    Solve 111

--- mundo_3/aula_22/divertimentos/desafio_111/desafio_111.py
+++ mundo_3/aula_22/divertimentos/desafio_111/desafio_111.py
@@ -1,7 +1,10 @@
 """
 Crie um pacote chamado utilidadesCeV que tenha dois módulos internos chamados moeda e dado.
-
 Transfira todas as funções utilizadas nos desafios 107, 108, 109 e 110 para o primeiro pacote e mantenha tudo funcionando.
 """
 
+from utilidadesCeV.moeda import resumo
 
+input_usuario = 558
+
+print(resumo(input_usuario, 20, 12))
