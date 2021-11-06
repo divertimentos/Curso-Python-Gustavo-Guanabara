c0dfc970d3d4a35f57fdb48e60afe2a16a680204
Guilherme Teixeira <24235344+vibraniumdev@users.noreply.github.com>
2021-11-06 15:05:38 -0300

    Melhora o 107 e cria as bases do 108

--- mundo_3/aula_22/divertimentos/desafio_107/desafio_107.py
+++ mundo_3/aula_22/divertimentos/desafio_107/desafio_107.py
@@ -11,10 +11,10 @@ o 'diminuir()', mesma coisa.
 
 import moeda
 
-# input_usuario = int(input("Digite o preço: \n"))
+# input_usuario = float(input("Digite o preço: \n"))
 input_usuario = 423
 
-print(f"A metade de R$ {input_usuario} é R$ {moeda.metade(input_usuario)}")
-print(f"O dobro de R$ {input_usuario} é R$ {moeda.dobro(input_usuario)}")
-print(f"Aumentando 10%, temos R$ {moeda.aumentar(input_usuario, 10)}")
-print(f"Diminuindo 10%, temos R$ {moeda.diminuir(input_usuario, 10)}")
+print(f"A metade de {input_usuario} é {moeda.metade(input_usuario)}")
+print(f"O dobro de {input_usuario} é {moeda.dobro(input_usuario)}")
+print(f"Aumentando 10%, temos {moeda.aumentar(input_usuario, 10)}")
+print(f"Diminuindo 10%, temos {moeda.diminuir(input_usuario, 10)}")
