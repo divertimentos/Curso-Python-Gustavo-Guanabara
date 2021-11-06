b608ed22e41f97c9703c4cb95594fb37ce365732
Guilherme Teixeira <24235344+vibraniumdev@users.noreply.github.com>
2021-11-06 15:19:52 -0300

    Resolve o 108

--- mundo_3/aula_22/divertimentos/desafio_108/desafio_108.py
+++ mundo_3/aula_22/divertimentos/desafio_108/desafio_108.py
@@ -1,3 +1,13 @@
 """
 Adapte o código do desafio 107, criando uma função adicional chamada 'moeda()' que consiga mostrar os valores como um valor monetário formatado.
 """
+
+import moeda
+
+# input_usuario = float(input("Digite o preço: \n"))
+input_usuario = 423
+
+print(f"A metade de R$ {input_usuario} é {moeda.moeda(moeda.metade(input_usuario))}")
+print(f"O dobro de R$ {input_usuario} é {moeda.moeda(moeda.dobro(input_usuario))}")
+print(f"Aumentando 10%, temos {moeda.moeda(moeda.aumentar(input_usuario, 10))}")
+print(f"Diminuindo 10%, temos {moeda.moeda(moeda.diminuir(input_usuario, 10))}")
--- mundo_3/aula_22/divertimentos/desafio_108/moeda.py
+++ mundo_3/aula_22/divertimentos/desafio_108/moeda.py
@@ -14,3 +14,11 @@ def aumentar(num, taxa):
 def diminuir(num, taxa):
     resultado = num - (taxa * num / 100)
     return resultado
+
+
+def moeda(num):
+    num_string = str(num)
+    splitted_num = num_string.split(".")
+    parte_inteira = splitted_num[0]
+    parte_decimal = splitted_num[-1]
+    return f"R$ {parte_inteira},{parte_decimal}"
