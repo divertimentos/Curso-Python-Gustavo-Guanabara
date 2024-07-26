bc23194763322ba95c47d97bff3b277b506e3583
Guilherme Teixeira <24235344+ggteixeira@users.noreply.github.com>
2024-07-25 23:56:01 -0300

    Finishes the class/lesson

--- mundo_3/aula_23/ambiente.py
+++ mundo_3/aula_23/ambiente.py
@@ -1,6 +1,29 @@
 from os import system
+system('clear')
 
-system("clear")
+# Exemplo 1
+# print(x)
 
-x = "oi"
-print(x)
+
+# Exemplo 2
+# n = int(input("Num: \n"))
+# print(f"Você digitou o número {n}.")
+
+# Exemplo 3
+a = int(input("Numerador: \n"))
+b = int(input("Demoninador: \n"))
+
+try:
+    r = a/b
+except (ValueError, TypeError):
+    print("Tivemos um problema com os tipos de dados digitados")
+except ZeroDivisionError:
+    print("Não é possível dividir um número por zero.")
+    print(f"O resultado, então, é {a}.")
+
+except Exception as error:
+    print(f"O erro encontrado foi {error.__class__}")
+else:
+    print(f"O resultado é {r:.1f}")
+finally:
+    print("\nVolte sempre! Muito obrigado!")
