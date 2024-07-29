7c85bdf80d9b66756813ccc026e50e2fdd36ccd3
Guilherme Teixeira <24235344+ggteixeira@users.noreply.github.com>
2024-07-29 10:33:41 -0300

    WIP

--- mundo_3/aula_23/divertimentos/desafio_113/desafio_113.py
+++ mundo_3/aula_23/divertimentos/desafio_113/desafio_113.py
@@ -2,17 +2,52 @@
 incluindo agora a possobilidade da digitação de um número de tipo inválido.
 Aproveite e crie também a função `leiaFloat()` com a mesma funcionalidade."""
 
+from os import system
 
-def leia_int(entrada_usuario):
+system("clear")
+
+
+# def leia_int_old(entrada_usuario):
+#     is_ok = False
+#     value = None
+#     while True:
+#         numero = input(f"{entrada_usuario} \n")
+#         if numero.isnumeric():
+#             value = numero
+#             is_ok = True
+#         else:
+#             print("\033[0;31mERRO! Digite um número inteiro válido!\033[m")
+#         if is_ok:
+#             break
+#     return value
+
+
+def leia_int_new():
     is_ok = False
-    value = None
+    int_final_value = None
+    float_final_value = None
     while True:
-        numero = input(f"{entrada_usuario} \n")
-        if numero.isnumeric():
-            value = numero
+        integer_user_input = input("Digite um número inteiro (int): \n")
+        float_user_input = float("Digite um número Real (float): \n")
+        try:
+            int(integer_user_input)
+            float(float_user_input)
+            int_final_value = integer_user_input
+            float_final_value = float_user_input
+
             is_ok = True
-        else:
+        except ValueError:
             print("\033[0;31mERRO! Digite um número inteiro válido!\033[m")
-        if is_ok:
-            break
-    return value
+
+        else:
+            if is_ok:
+                break
+    return f"""O valor inteiro digitado foi {int_final_value}
+    e o real foi {float_final_value}"""
+
+
+# number = leia_int_old("Digite um número:")
+# print(f"Você acabou de digitar o número: {number}.")
+
+
+print(leia_int_new())
